caa5bf9640c11c25dcee6c66080bbf53ebf659aa
Adam Gulczynski <adamg@example.com>
2023-07-10 00:33:40 +0200

    create CRUD for job_skills table (#9)

---
+++ db/queries/job_skill.sql
@@ -0,0 +1,27 @@
+-- name: CreateJobSkill :one
+INSERT INTO job_skills (job_id, skill)
+VALUES ($1, $2)
+RETURNING *;
+
+-- name: ListJobSkillsByJobID :many
+SELECT id, skill
+FROM job_skills
+WHERE job_id = $1
+LIMIT $2 OFFSET $3;
+
+-- name: ListJobsBySkill :many
+SELECT job_id
+FROM job_skills
+WHERE skill = $1
+LIMIT $2 OFFSET $3;
+
+-- name: UpdateJobSkill :one
+UPDATE job_skills
+SET skill = $2
+WHERE id = $1
+RETURNING *;
+
+-- name: DeleteJobSkill :exec
+DELETE
+FROM job_skills
+WHERE id = $1;
--- db/sqlc/querier.go
+++ db/sqlc/querier.go
@@ -11,25 +11,30 @@ import (
 type Querier interface {
 	CreateCompany(ctx context.Context, arg CreateCompanyParams) (Company, error)
 	CreateJob(ctx context.Context, arg CreateJobParams) (Job, error)
+	CreateJobSkill(ctx context.Context, arg CreateJobSkillParams) (JobSkill, error)
 	CreateUser(ctx context.Context, arg CreateUserParams) (User, error)
 	DeleteCompany(ctx context.Context, id int32) error
 	DeleteJob(ctx context.Context, id int32) error
+	DeleteJobSkill(ctx context.Context, id int32) error
 	DeleteUser(ctx context.Context, id int32) error
 	GetCompanyByID(ctx context.Context, id int32) (Company, error)
 	GetCompanyByName(ctx context.Context, name string) (Company, error)
 	GetJob(ctx context.Context, id int32) (Job, error)
 	GetUserByEmail(ctx context.Context, email string) (User, error)
 	GetUserByID(ctx context.Context, id int32) (User, error)
+	ListJobSkillsByJobID(ctx context.Context, arg ListJobSkillsByJobIDParams) ([]ListJobSkillsByJobIDRow, error)
 	ListJobsByCompanyExactName(ctx context.Context, arg ListJobsByCompanyExactNameParams) ([]Job, error)
 	ListJobsByCompanyID(ctx context.Context, arg ListJobsByCompanyIDParams) ([]Job, error)
 	ListJobsByCompanyName(ctx context.Context, arg ListJobsByCompanyNameParams) ([]Job, error)
 	ListJobsByIndustry(ctx context.Context, arg ListJobsByIndustryParams) ([]Job, error)
 	ListJobsByLocation(ctx context.Context, arg ListJobsByLocationParams) ([]Job, error)
 	ListJobsBySalaryRange(ctx context.Context, arg ListJobsBySalaryRangeParams) ([]Job, error)
+	ListJobsBySkill(ctx context.Context, arg ListJobsBySkillParams) ([]int32, error)
 	ListJobsByTitle(ctx context.Context, arg ListJobsByTitleParams) ([]Job, error)
 	ListJobsMatchingUserSkills(ctx context.Context, arg ListJobsMatchingUserSkillsParams) ([]Job, error)
 	UpdateCompany(ctx context.Context, arg UpdateCompanyParams) (Company, error)
 	UpdateJob(ctx context.Context, arg UpdateJobParams) (Job, error)
+	UpdateJobSkill(ctx context.Context, arg UpdateJobSkillParams) (JobSkill, error)
 	UpdatePassword(ctx context.Context, arg UpdatePasswordParams) error
 	UpdateUser(ctx context.Context, arg UpdateUserParams) (User, error)
 }
